e8b46fbfe3b4a53bfe7c70b8200c9ac9a0d0ab02
Rasmi Jati <jatirasmi65@gmail.com>
2023-08-22 15:30:12 +0545

    created model and view for country and added getcountry method in home controller

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,32 +1,38 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
 using System.Diagnostics;
 using WebApi.Models;
 
 namespace WebApi.Controllers
-{
-    public class HomeController : Controller
-    {
-        private readonly ILogger<HomeController> _logger;
-
-        public HomeController(ILogger<HomeController> logger)
+{   
+        public class HomeController : Controller
         {
-            _logger = logger;
-        }
+            private readonly ILogger<HomeController> _logger;
 
-        public IActionResult Index()
-        {
-            return View();
-        }
+            public HomeController(ILogger<HomeController> logger)
+            {
+                _logger = logger;
+            }
 
-        public IActionResult Privacy()
-        {
-            return View();
-        }
+            public IActionResult Index()
+            {
+                return View();
+            }
 
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+            public async Task<IActionResult> GetCountry()
+            {
+                HttpClient client = new HttpClient();
+                using HttpResponseMessage response = await client.GetAsync("https://restcountries.com/v3.1/all");
+                response.EnsureSuccessStatusCode();
+                string responseBody = await response.Content.ReadAsStringAsync();
+                var data = JsonConvert.DeserializeObject<List<Country>>(responseBody);
+                return View(data);
+            }
+
+            [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+            public IActionResult Error()
+            {
+                return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+            }
         }
     }
-}
