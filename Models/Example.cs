ea8f53fdd6c7288ed3adc38d9e767cdf547389e7
Rasmi Jati <jatirasmi65@gmail.com>
2023-08-24 11:01:45 +0545

    made generic for name and currency

---
+++ Models/Example.cs
@@ -0,0 +1,39 @@
+﻿//namespace WebApi.Models
+//{
+//    public class Example
+//    {
+//        public Name name { get; set; }
+//        public string[] tld { get; set; }
+//        public string cca2 { get; set; }
+//        public string ccn3 { get; set; }
+//        public string cca3 { get; set; }
+//        public string cioc { get; set; }
+//        public bool independent { get; set; }
+//        public string status { get; set; }
+//    }
+
+//    public class GenericName<T1, T2, T3>
+//    {
+//        public T1 t1 { get; set; }
+//        public T2 t2 { get; set; }
+//        public T3 t3 { get; set; }
+//    }
+
+//    public class GenericNativeName<T>
+//    {
+//        public T t1 { get; set; }
+//    }
+
+//    public class Name : GenericName<string,string,NativeName> 
+//    {
+//        public string common { get; set; }
+//        public string official { get; set; }
+//        public Nativename nativee { get; set; }
+//    }
+
+//    public class Nativename : GenericNativeName<string>
+//    {
+//        public string common { get; set; }
+//        public string official { get; set; }
+//    }
+//}
---
+++ Models/GenericCountry.cs
@@ -0,0 +1,11 @@
+﻿public class GenericName<T1, T2, T3>
+{
+    public T1 t1 { get; set; }
+    public T2 t2 { get; set; }
+    public T3 t3 { get; set; }
+}
+
+public class GenericNativeName<T>
+{
+    public T t1 { get; set; }
+}
